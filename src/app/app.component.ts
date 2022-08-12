ffbee02d70eae3dbc237a114aedf3c01e416e452
ismailharik <ismailharik77@gmail.com>
2022-08-12 21:57:49 +0100

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Toaster';
+  show: boolean=false;
+  alertProgress: number=0;
+
+  showAlert(){
+    this.show=true;
+    let myInterval=setInterval(()=>{
+      this.alertProgress+=10;
+      // YOU CAN ADJUST DELAI
+    },300)
+    setTimeout(()=>{
+      this.show=false
+      
+      //stop increasing progress
+      clearInterval(myInterval);
+      this.alertProgress=0;
+
+    },3000)
+  }
+
+
 }
